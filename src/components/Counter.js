a61667c51151351804189dc9edd3cb5867a9c38d
tinyuliu <tinyu1016@gmail.com>
2021-07-10 15:26:46 +0800

    v8 migrating ecerything to redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,27 @@
 import { useSelector, useDispatch } from 'react-redux';
 
+import { counterActions } from '../store/index';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
-  //counter is managed by Redux
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
-
-  const increamentHandler = () => {
-    dispatch({type: 'increment'});
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({type:'increase', amount: 5});
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 10 }
   };
 
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   };
 
-
   const toggleCounterHandler = () => {
-    dispatch({type:'toggle'});
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
@@ -31,9 +29,9 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={increamentHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increse by 5</button>
-        <button onClick={decrementHandler}>decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,30 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true}
+const initialState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
-        increment(state){
-            state.counter++;
-        },
-        decrement(state){
-            state.counter--;
-        },
-        increse(state, action) {
-            state.counter = state.counter + action.amount;
-        },
-        toggleCounter(state) {
-            state.showCounter
-        }
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
     }
-})
-
+  }
+});
 
 const store = configureStore({
-    reducer: {
-        counter: counterSlice.reducer
-    }
+  reducer:counterSlice.reducer
 });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
