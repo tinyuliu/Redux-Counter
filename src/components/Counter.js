7684def4d7702a794883ddbe0add0430bfe2626a
tinyuliu <tinyu1016@gmail.com>
2021-07-10 12:06:29 +0800

    v3 bind dispatch functoin

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+
+  //counter is managed by Redux
+  const counter = useSelector(state => state.counter);
+
+  const increamentHandler = () => {
+    dispatch({type: 'increment'});
+  };
+
+  const decrementHandler = () => {
+    dispatch({type: 'decrement'});
+  };
+
+
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={increamentHandler}>Increment</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
